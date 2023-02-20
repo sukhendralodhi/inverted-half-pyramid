39fa9054e7d460fda72248032cdedec344870f03
Sukhendra S Lodhi <95135906+sukhendralodhi@users.noreply.github.com>
2023-02-20 12:38:56 +0000

    add function in that code

--- .ccls-cache/@home@runner@inverted-half-pyramid/main.cpp
+++ .ccls-cache/@home@runner@inverted-half-pyramid/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  int n = 5;
-
+void starPattern(int n) {
   for (int i = n; i >= 1; i--) {
     for (int j = 1; j <= i; j++) {
       cout << "*";
     }
     cout << endl;
   }
+}
+
+int main() {
+  int n;
+  cout << "Enter how many stars inverted half pyramid pattern you want: ";
+  cin >> n;
+  starPattern(n);
 }
